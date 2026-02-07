934032bc64f04cd3b3fabb5625a6e990388fdc53
Samarth Raj <samarthraj@Mac.lan>
2026-02-07 21:05:32 +0530

    Impliment Art Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Cocktails from "./components/Cocktails"
+import About from "./components/About"
+import Art from "./components/Art"
 
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
@@ -10,7 +13,9 @@ const App = () => {
    <main>
     <Navbar/>
     <Hero/>
-    <div className="h-dvh bg-black"/>
+    <Cocktails/>
+    <About/>
+    <Art/>
    </main>
   )
 }
